4e7021f4b4ac2898638bc49734875f6f183430b5
Arsalan Malik <119423613+arsalanmalik7@users.noreply.github.com>
2024-06-03 13:56:24 +0500

    Update core.mjs

--- frontend/src/core.mjs
+++ frontend/src/core.mjs
@@ -1,3 +1,3 @@
 const isProduction = window.location.href.includes("localhost");
 
-export const baseUrl = isProduction ? "http://localhost:3001" : "http://localhost:3001";
+export const baseUrl = isProduction ? "http://localhost:3001" : "https://social-link-cghp.onrender.com";
